09960d9d04f2e14549bc83f77b2543af305d7731
10632185 <10632185@ucn.dk>
2026-05-22 12:28:20 +0200

    Fixed last setup of nuxt + wordpress
    * Wordpress posts are now being pulled
    * Nuxt is setup correctly

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,38 @@
+<script setup>
+const { data: posts, pending, error } = await useFetch(
+  'https://xn--lynghjsolutions-9tb.dk/wp-json/wp/v2/posts',
+  {
+    server: true,
+    lazy: false,
+    default: () => []
+  }
+)
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>WordPress Posts</h1>
+
+    <p v-if="pending">Loading...</p>
+
+    <p v-else-if="error">
+      Kunne ikke hente data fra WordPress
+    </p>
+
+    <div v-else>
+      <div v-for="post in posts" :key="post.id">
+        <h2 v-html="post.title.rendered"></h2>
+
+        <p v-if="post.acf?.hero_description">
+          {{ post.acf.hero_description }}
+        </p>
+
+        <img
+          v-if="post.acf?.hero_img?.url"
+          :src="post.acf.hero_img.url"
+          alt=""
+        />
+      </div>
+    </div>
   </div>
 </template>
